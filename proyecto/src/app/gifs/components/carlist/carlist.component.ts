5961f968676b8a71a47ed848d124045cdd23892f
ITZUK1 <128190067+ITZUK1@users.noreply.github.com>
2024-09-06 14:14:18 -0500

    pagina funcionando

---
+++ proyecto/src/app/gifs/components/carlist/carlist.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-card-list',
+  templateUrl: './carlist.component.html',
+})
+export class CarlistComponent {
+
+}
--- proyecto/src/app/gifs/gifs.module.ts
+++ proyecto/src/app/gifs/gifs.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HomeComponent } from './pages/home/home-page.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { CarlistComponent } from './components/carlist/carlist.component';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    SearchBoxComponent,
+    CarlistComponent,
+  ],
   imports: [
-    CommonModule
+    CommonModule 
+  ],
+  exports:[
+    HomeComponent,
   ]
 })
 export class GifsModule { }
