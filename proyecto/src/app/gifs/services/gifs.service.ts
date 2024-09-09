c8559c7c450d3fc601b0f6fa01f0f763302ab830
ITZUK1 <128190067+ITZUK1@users.noreply.github.com>
2024-09-09 09:48:59 -0500

    http

--- proyecto/src/app/gifs/services/gifs.service.ts
+++ proyecto/src/app/gifs/services/gifs.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from "@angular/core";
 
+
+
 @Injectable ({providedIn: 'root' })
 export class GifsService{
 
     _tagsHistory: string[] = [];
+
+    private apikey: string ='Y00w9FLELLMmD0ofcadzfiB43vednajD';
 
 
     constructor() {}
@@ -25,11 +29,14 @@ export class GifsService{
     }
 
 
-    searchTag (tag: string):void{
+      async searchTag (tag: string):Promise<void>{
 
         if (tag.length === 0) return;
         this.organizeHistory(tag);
 
+       /*  const resp = await fetch('https://api.giphy.com/v1/gifs/search?api_key=Y00w9FLELLMmD0ofcadzfiB43vednajD&q=valorant&limit=10')
+        const data =resp.json();
+        console.log(data) */
     }
 
 
